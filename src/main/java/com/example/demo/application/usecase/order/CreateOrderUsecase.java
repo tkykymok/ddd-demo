c8e4793db8674e50f7682b444b6e0740914896be
tkykymok <tkykymok@me.com>
2023-12-26 17:09:44 +0900

    一旦保存

--- src/main/java/com/example/demo/application/usecase/order/CreateOrderUsecase.java
+++ src/main/java/com/example/demo/application/usecase/order/CreateOrderUsecase.java
@@ -24,6 +24,7 @@ public class CreateOrderUsecase extends Usecase<CreateOrderInput, Void> {
     private final OrderItemRepository orderItemRepository;
     private final ProductRepository productRepository;
 
+    // コンストラクタでリポジトリの初期化
     public CreateOrderUsecase(OrderRepository orderRepository, OrderItemRepository orderItemRepository, ProductRepository productRepository) {
         this.orderRepository = orderRepository;
         this.orderItemRepository = orderItemRepository;
@@ -34,12 +35,16 @@ public class CreateOrderUsecase extends Usecase<CreateOrderInput, Void> {
     @Transactional
     public Void execute(CreateOrderInput input) {
 
+        // オーダーを生成し、永続化する
         Order createdOrder = createAndPersistOrder(input.userId());
 
+        // オーダーアイテムの商品IDを抽出する
         List<ProductId> productIds = extractProductIdsFromOrder(input);
 
+        // 商品IDから商品をフェッチする
         Map<ProductId, Product> products = fetchProductsByIds(productIds);
 
+        // オーダーアイテムを追加し、全体を永続化する
         input.orderItems()
                 .forEach(orderItem -> {
                     createdOrder.addOrderItem(
@@ -53,17 +58,21 @@ public class CreateOrderUsecase extends Usecase<CreateOrderInput, Void> {
         return null;
     }
 
+    // オーダーを生成し、永続化するメソッド
     private Order createAndPersistOrder(Long userId) {
         Order order = Order.create(new UserId(userId));
         return orderRepository.save(order);
     }
 
+
+    // 商品IDをオーダから抽出するメソッド
     private List<ProductId> extractProductIdsFromOrder(CreateOrderInput input) {
         return input.orderItems().stream()
                 .map(orderItem -> new ProductId(orderItem.productId()))
                 .toList();
     }
 
+    // 商品IDから商品をフェッチするメソッド
     private Map<ProductId, Product> fetchProductsByIds(List<ProductId> productIds) {
         return productRepository.findAllById(productIds).stream()
                 .collect(Collectors.toMap(product -> new ProductId(product.getId().value()), product -> product));
--- src/main/java/com/example/demo/domain/model/order/Order.java
+++ src/main/java/com/example/demo/domain/model/order/Order.java
@@ -12,45 +12,46 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 @Getter
-@Table(name = "orders")
+@Table(name = "orders") // "orders"という名前のテーブルとマッピングします
 public class Order extends AggregateRoot<OrderId>{
-    private UserId userId;
-    private LocalDate orderDate;
-    private Amount totalAmount;
+    private UserId userId; // ユーザーID
+    private LocalDate orderDate; // 注文日
+    private Amount totalAmount; // 合計金額
     @Version
-    private Long version;
-    @MappedCollection(idColumn = "order_id")
-    private Set<OrderItem> orderItems;
+    private Long version; // バージョン
+    @MappedCollection(idColumn = "order_id") // "order_id"を識別子とするマッピングコレクション
+    private Set<OrderItem> orderItems; // 注文アイテムのセット
 
-    private Order() {}
+    private Order() {} // プライベートコンストラクタ
 
-    public static Order create(UserId userId) {
+    public static Order create(UserId userId) { // Orderを新規作成するスタティクメソッド
         Order order = new Order();
-        order.userId = userId;
-        order.orderDate = LocalDate.now();
-        order.totalAmount = Amount.of(0);
-        order.orderItems = new LinkedHashSet<>();
-        return order;
+        order.userId = userId; // ユーザーIDを設定します
+        order.orderDate = LocalDate.now(); // 注文日に現在の日付を設定します
+        order.totalAmount = Amount.of(0); // 合計金額を0に初期設定します
+        order.orderItems = new LinkedHashSet<>(); // OrderItemのセットを新規作成します
+        return order; // Orderを返します
     }
 
-    public void addOrderItem(OrderItemId id, Product product, Quantity quantity) {
-        SeqNo seqNo = SeqNo.of(this.orderItems.size() + 1);
+    public void addOrderItem(OrderItemId id, Product product, Quantity quantity) { // OrderItemを追加するメソッド
+        SeqNo seqNo = SeqNo.of(this.orderItems.size() + 1); // SeqNoを作成します
 
+        // 注文アイテムを作成または更新します
         OrderItem orderItem = id.value() == null
                 ? OrderItem.create(this.getId(), seqNo, product, quantity)
                 : OrderItem.update(id, this.getId(), seqNo, product, quantity);
-        this.orderItems.add(orderItem);
-        calculateTotalAmount();
+        this.orderItems.add(orderItem); // 注文アイテムを追加します
+        calculateTotalAmount(); // 合計金額を計算します
     }
 
-    public void clearOrderItems() {
-        this.orderItems.clear();
+    public void clearOrderItems() { // 注文アイテムを全てクリアするメソッド
+        this.orderItems.clear(); // 注文アイテムをクリアします
     }
 
-    private void calculateTotalAmount() {
-        this.totalAmount = Amount.of(0);
-        for (OrderItem orderItem : this.orderItems) {
-            this.totalAmount = this.totalAmount.add(orderItem.getSubTotalAmount());
+    private void calculateTotalAmount() { // 合計金額を計算するメソッド
+        this.totalAmount = Amount.of(0); // 合計金額を0に初期化します
+        for (OrderItem orderItem : this.orderItems) { // 注文アイテムすべてに対して
+            this.totalAmount = this.totalAmount.add(orderItem.getSubTotalAmount()); // 各アイテムの小計を合計金額に加算します
         }
     }
 
